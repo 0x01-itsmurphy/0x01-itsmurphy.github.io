9d76deb0d5cd551f3b8f467574e6d3b56bde4d17
itsmurphy <mass.kumar.186@gmail.com>
2021-06-10 02:44:50 +0530

    navbar linkToSection header bgCircles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
+/*------------------------------------- Toggle Navbar ---------------------------------*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
 
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
 
-/*------------------------------------- About Tabs ---------------------------------*/ 
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+/*------------------------------------- Active Section ---------------------------------*/
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        //to prevent multiple clicks activate overlay
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if (e.target.classList.contains("nav-item")) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0, 0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        }, 500);
+    }
+})
+
+/*------------------------------------- About Tabs ---------------------------------*/
 const tabsContainer = document.querySelector(".about-tabs"),
-aboutSection = document.querySelector(".about-section");
+    aboutSection = document.querySelector(".about-section");
 
-tabsContainer.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+tabsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
         tabsContainer.querySelector(".active").classList.remove("active");
         e.target.classList.add("active");
         const target = e.target.getAttribute("data-target");
@@ -16,15 +53,16 @@ tabsContainer.addEventListener("click", (e) =>{
 });
 
 
-/*-------------------------- Portfolio Item Details Popup -----------------------*/ 
-document.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("view-project-btn")){
+/*-------------------------- Portfolio Item Details Popup -----------------------*/
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-project-btn")) {
         togglePortfolioPopup();
-        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        document.querySelector(".portfolio-popup").scrollTo(0, 0);
         portfolioItemDetails(e.target.parentElement);
     }
 })
-function togglePortfolioPopup(){
+
+function togglePortfolioPopup() {
     document.querySelector(".portfolio-popup").classList.toggle("open");
     document.body.classList.toggle("hide-scrolling");
     document.querySelector(".main").classList.toggle("fade-out");
@@ -34,18 +72,18 @@ document.querySelector(".pp-close").addEventListener("click", togglePortfolioPop
 
 // hide popup when clicking outside of it
 document.addEventListener("click", (e) => {
-    if(e.target.classList.contains("pp-inner")){
+    if (e.target.classList.contains("pp-inner")) {
         togglePortfolioPopup();
     }
 });
 
-function portfolioItemDetails(portfolioItem){
+function portfolioItemDetails(portfolioItem) {
     document.querySelector(".pp-thumbnail img").src =
-    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+        portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
 
     document.querySelector(".pp-header h3").innerHTML =
-    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+        portfolioItem.querySelector(".portfolio-item-title").innerHTML;
 
     document.querySelector(".pp-body").innerHTML =
-    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+        portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
